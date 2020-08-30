fcc514bf1e757731dd723ce6c7d1e3f28e022f0d
s_mori <s_mori@aidma-hd.jp>
2020-08-31 08:52:13 +0900

    deferでファイルを閉じるため読み込み&表示をクロージャにまとめる

--- 5/cat_cmd/mycat.go
+++ 5/cat_cmd/mycat.go
@@ -17,15 +17,17 @@ func main() {
 
 	// コマンドの引数をrangeで回してファイルを開く
 	rowNumber := 1
-	for _, filepath := range files {
+	printlnFile := func(filepath string, isShowRowNumber bool) {
 		fp, err := os.Open(filepath)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "ファイルを開くことができませんでした:", err)
 			os.Exit(1)
 		}
+		defer fp.Close()
+
 		scanner := bufio.NewScanner(fp)
 		for scanner.Scan() {
-			if *isShowRowNumber {
+			if isShowRowNumber {
 				fmt.Fprintln(os.Stdout, fmt.Sprint(rowNumber, ":"), scanner.Text())
 			} else {
 				fmt.Fprintln(os.Stdout, scanner.Text())
@@ -36,7 +38,10 @@ func main() {
 			fmt.Fprintln(os.Stderr, "読み込みに失敗しました:", err)
 			os.Exit(1)
 		}
-		fp.Close() // todo: deferを使ってファイルを閉じる
 
+	}
+
+	for _, filepath := range files {
+		printlnFile(filepath, *isShowRowNumber)
 	}
 }
